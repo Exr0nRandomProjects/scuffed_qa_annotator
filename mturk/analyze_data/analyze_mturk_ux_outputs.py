343f453bf3cfc56062496e392665c3ba1f5e5bea
Exr0n <mail@exr0n.com>
2021-11-10 06:07:26 -0800

    how to calculate annotator agreement?

--- mturk/analyze_data/analyze_mturk_ux_outputs.py
+++ mturk/analyze_data/analyze_mturk_ux_outputs.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-FILENAMES = ['triple_batch.csv', 'second_time.csv']
+# FILENAMES = ['triple_batch.csv', 'second_time.csv']
+FILENAMES = ['triple_batch.csv']
+
+LABELS = { "Definetly incorrect": 1, "I dont think its correct": 2, "Unsure": 3, "I think its correct": 4, "Definitely correct": 5  }
 
 # read all data
 info_dataframes = []
@@ -12,9 +15,11 @@ for file in FILENAMES:
 # calculate overlap statistics
 all_dfs = pd.concat(info_dataframes)
 all_dfs = all_dfs.reset_index(drop=True)
+all_dfs['worker_score'] = all_dfs['Answer.sentiment.label'].map(LABELS)
+print(all_dfs['Answer.sentiment.label'])
 grouped = all_dfs.groupby('Input.qap_id')
-grouped = grouped.agg({'WorkerId': 'nunique'})
-grouped = grouped[grouped['WorkerId'] == 3]
+# print(grouped['worker_score'].range())
+# print(grouped['worker_score'].std())
 # all_dfs = all_dfs.sort_values(by=['Input.qap_id'])
 # print(all_dfs)
-print(grouped)
+# print(grouped)
